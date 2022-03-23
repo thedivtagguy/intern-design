c0450b3fff26644e9586dbd63a2a66498f5d0cb2
Aman Bhargava <amanbhargava2001@gmail.com>
2022-03-23 23:33:15 +0530

    Comment Meta Tags

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -41,5 +41,5 @@
   <!-- <link rel="apple-touch-icon" sizes="180x180" href="/common/assets/resources/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/common/assets/resources/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/common/assets/resources/favicon/favicon-16x16.png"> -->
-  <link rel="manifest" href="/common/assets/resources/favicon/site.webmanifest">
+  <!-- <link rel="manifest" href="/common/assets/resources/favicon/site.webmanifest"> -->
 </svelte:head>
